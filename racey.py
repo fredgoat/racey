58b8be8df716fbe765bd41b7cb2715a95d14debc
fredgoat <fredgoat@gmail.com>
2018-01-11 23:14:20 -0800

    Car motion
    Motioning the car!

--- racey.py
+++ racey.py
@@ -28,7 +28,16 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
-        print(event)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+
+    x += x_change
 
     gameDisplay.fill(white)
     car(x,y)
