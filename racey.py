a9d794f50f62afc59f4dff94d9af7c64ee7c00f2
fredgoat <fredgoat@gmail.com>
2018-01-18 16:20:27 -0800

    Smallified the car, turned "crashed = False" into "driving = True", debugged everything, and got an actual font for "You Crashed"

--- racey.py
+++ racey.py
@@ -13,7 +13,7 @@ pygame.display.set_caption('A bit Racey')
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
-car_width = 73
+car_width = 150
 carImg = pygame.image.load('racecar.png')
 
 def car(x,y):
@@ -24,38 +24,42 @@ def text_objects(text, font):
     return textSurface, textSurface.get_rect()
 
 def message_display(text):
-    largeText = pygame.font.Font('freesansbold.tff',115)
+    largeText = pygame.font.Font('wedgie.ttf',80)
     TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
+    TextRect.center = ((display_width/2),(display_height/3))
     gameDisplay.blit(TextSurf, TextRect)
 
     pygame.display.update()
 
     time.sleep(2)
 
-def crash():
+driving = True
+
+def crash(state):
     message_display('You Crashed')
-    game_loop()
+    pygame.time.wait(1000)
+    print "debug crashed"
+    return state
 
 
-def game_loop():
-    x =  (display_width * 0.45)
-    y = (display_height * 0.8)
-
+def game_loop(state):
+    x =  (display_width * 0.4)
+    y = (display_height * 0.45)
     x_change = 0
+    print "debug loop"
 
-    gameExit = False
-
-    while not crashed:
+    while state:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print "debug eventquit"
                 pygame.quit()
                 quit()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
+                    print "debug left"
                 elif event.key == pygame.K_RIGHT:
                     x_change = 5
             if event.type == pygame.KEYUP:
@@ -68,10 +72,12 @@ def game_loop():
         car(x,y)
 
         if x > display_width - car_width or x < 0:
-            crash()
+            print "debug crash initiation"
+            crash(state)
 
         pygame.display.update()
         clock.tick(60)
 
+game_loop(driving)
 pygame.quit()
 quit()
